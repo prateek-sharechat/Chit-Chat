cad05fa2d6a8b8c57d69e0558071e35e2252e8e8
prateekshukla <prateekshukla@sharechat.co>
2021-01-24 13:21:41 +0530

    deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const container = require('./di');
 const server = require('./server');
 const bodyParser = require("body-parser")
-const mongo = require('../backend/driver/db-helper')
+const mongo = require('./driver/db-helper')
 
 server(container).then(async (app) => {
 	const config = container.resolve('serverConfig');
